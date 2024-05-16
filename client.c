9c12e47b69c4833e30c4dd348bfc28b3e547ffa4
Roman Njoroge <roman.njoroge@njuguna.com>
2024-05-16 20:37:36 +0300

    feat: Algorithm for creating TCP client

---
+++ client.c
@@ -0,0 +1,18 @@
+int main() {
+    // Define IP address of server you want to communicate with (target server)
+    //
+    // Define port number used by target server
+    //
+    // Create socket
+    //
+    // Allow TCP to dynamically choose port client will use
+    //
+    // Connect socket to server
+    //
+    // Loop
+    // Send a request (use our defined message format
+    //
+    // Await response
+    //
+    // Close the connection
+}
