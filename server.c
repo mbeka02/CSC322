cfabeb77dff4a3ded33aa2c1031702e5dbdbb4b4
Roman Njoroge <roman.njoroge@njuguna.com>
2024-05-16 19:50:10 +0300

    feat: Create algorithm for creating server

---
+++ server.c
@@ -0,0 +1,17 @@
+int main() {
+    // Create socket
+    int serverSocketFD = socket()
+    // Bind to well known address
+    //
+    // Place connection in listen mode
+    //
+    // Obtain a new socket for connection when a client connects
+    //
+    // Read request from client
+    //
+    // Formulate response
+    //
+    // Send reply
+    //
+    // When finished with client close connection
+}
