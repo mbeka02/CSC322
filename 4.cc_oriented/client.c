eb9bdc72b400371b1b7f02fbda920ef51bbfb854
mbeka02 <antonymbeka@gmail.com>
2024-05-18 20:12:57 +0300

    init cc_oriented

---
+++ 4.cc_oriented/client.c
@@ -0,0 +1,116 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#include "helpers.h"
+#define BUFSIZE 1024
+
+int main() {
+    struct sockaddr_in servaddr, cli;
+    // Define IP address of server you want to communicate with (target server)
+    char * serverIPAddress = "127.0.0.1";
+ 
+    // Define port number used by target server
+    int serverPortNumber = 3000;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(serverIPAddress);
+    servaddr.sin_port = htons(serverPortNumber);
+
+    // Create socket
+    int clientSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocketFD < 0)
+    {
+        perror("Error opening socket");
+        exit(1);
+    }
+
+    // Allow TCP to dynamically choose port client will use (so don't bind)
+    
+    // Connect socket to server
+
+    // connect the client socket to server socket
+    if (connect(clientSocketFD, (const struct sockaddr*)&servaddr, sizeof(servaddr))
+        != 0) {
+        perror("connection with the server failed...\n");
+        exit(0);
+    }
+
+    bool stopLooping = false;
+    char *message = malloc(sizeof(char) * 500);
+
+    // Loop
+    while(stopLooping == false) {
+        // Let user choose what they want
+        DisplayMenu(MAX_LINE_LENGTH);
+        int menu_item;
+        printf("Select an item from the menu: ");
+        // Get user input
+        scanf("%d", &menu_item);
+        // switch case for each option
+        switch (menu_item) {
+            case 1:
+                printf("...displaying catalog\n");
+                message = "1";
+                // DisplayCatalog();
+                break;
+            case 2:
+                printf("Enter the title of the book you want to search for: ");
+                char title[100];
+                scanf("%s", title);
+                
+                strcat(message, "2; ");
+                strcat(message, title);
+
+                /* char* search_result = searchInFile(title); */
+                /* if (strcmp(search_result, "") == 0) { */
+                /*     printf("Book Not Found\n"); */
+                /* } else { */
+                /*     printf("Book Found!\n"); */
+                /* } */
+                break;
+            case 3:
+                // PurchaseItem();
+                message = "3";
+                break;
+            case 4:
+                // PayForItem();
+                message = "4";
+                break;
+            case 5:
+                printf("Thanks for shopping with us\n");
+                message = "5";
+                stopLooping = true;
+                break;
+            default:
+                printf("Invalid option\n");
+                break;
+        }
+
+        // Send a request (use our defined message format)
+        int isWritten = write(clientSocketFD, message, strlen(message));
+
+        if (isWritten == -1) {
+            perror("Could Not Send Data\n");
+            exit(2);
+        }
+
+        // Await response
+        // Create buffer to hold server response
+        char buffer[BUFSIZE];
+        // Get response
+        bzero(buffer, BUFSIZE); 
+        // read the message from client and copy it in buffer 
+        read(clientSocketFD, buffer, sizeof(buffer));
+        // Print Response
+        printf("\nServer responded with %s\n", buffer);
+    }
+
+    // Close the connection
+    close(clientSocketFD);
+    printf("Client socket closed\n"); 
+}
---
+++ 4.cc_oriented/server.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include "data.h"
+#include "helpers.h"
+# define PORT 3000
+#define BUFFERSIZE 1024
+
+void *handle_client(void *arg){
+int sockFd= *(int *)arg;
+struct sockaddr_in clientaddr; /* client addr */
+int clientlen=sizeof(clientaddr);/* byte size of client's address */
+ssize_t recv_len;
+char buffer[BUFFERSIZE];
+
+
+while(1){
+    struct Data data;
+
+    recv_len=recvfrom(sockFd,buffer,BUFFERSIZE,0,(struct sockaddr *)&clientaddr,&clientlen);
+if(recv_len < 0){
+      perror("recvfrom");
+      break;
+    }
+  struct Data *incoming_data=(struct Data *)buffer;
+  char *response = "...received";
+  sendto(sockFd, response, strlen(response), 0, (struct sockaddr *)&clientaddr,clientlen);
+  }
+    
+  free(arg);//free socket descriptor mem
+  
+close(sockFd);
+pthread_exit(NULL);
+return NULL;
+}
+
+/*int main (){
+//The server socket id 
+  int sockFd , ret;
+struct sockaddr_in serveraddr; server's addr 
+struct sockaddr_in clientaddr;  client addr  
+int clientlen;                 byte size of client's address  
+int childfd;                    child socket 
+struct hostent *hostp;          client host info 
+char buf[BUFSIZE];              message buffer 
+char *hostaddrp;                dotted decimal host addr string 
+int n;                          message byte size 
+// Child process id
+    pid_t childpid;
+    
+
+    // Create socket
+    sockFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFd < 0)
+    {
+        perror("Error opening socket");
+        exit(1);
+    }
+    // Bind to well known address
+
+    
+    // build the server's Internet address
+     
+    bzero((char *)&serveraddr, sizeof(serveraddr));
+    // this is an Internet address 
+    serveraddr.sin_family = AF_INET;
+
+    // let the system figure out our IP address 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // this is the port we will listen on 
+    serveraddr.sin_port = htons((unsigned short)PORT);
+
+    
+     // bind: associate the parent socket with a port
+     
+    if (bind(sockFd, (struct sockaddr *)&serveraddr,
+             sizeof(serveraddr)) < 0)
+    {
+        perror("ERROR on binding");
+        exit(2);
+    }
+
+    //
+    // Place connection in listen mode
+    //
+    
+     //listen: make this socket ready to accept connection requests
+     
+    if (listen(sockFd, 10) == 0) // allow 5 requests to queue up 
+    {
+        perror("ERROR on listen");
+        exit(3);
+    }
+  int count=0;
+
+    // Obtain a new socket for connection when a client connects
+    //
+    // main loop: wait for a connection request, echo input line,then close connection.
+  
+    clientlen = sizeof(clientaddr);
+ while(1){
+printf("server waiting for connection...\n");
+     childfd = accept(sockFd, (struct sockaddr *)&clientaddr, &clientlen);
+        if (childfd < 0) {
+            perror("ERROR on accept");
+            exit(4);
+        }
+printf("accepted connection from %s:%d\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
+
+printf("%d clients connected \n",count++);
+if((childpid =fork())==0){
+      close(sockFd);
+    }
+  }
+}*/
+int main (){
+
+  int sockFd , *client_sock;
+  struct sockaddr_in server_addr,clientaddr;
+  pthread_t thread_id;
+ int clientlen = sizeof(clientaddr);
+
+  sockFd=socket(AF_INET,SOCK_STREAM,0);
+  if (sockFd==-1){
+    perror("Failed to create the socket");
+    exit(EXIT_FAILURE);
+  }
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr=INADDR_ANY;
+  server_addr.sin_port=htons(PORT);
+
+  //binding 
+  if(bind(sockFd,(struct sockaddr *)&server_addr , sizeof(server_addr))==-1){
+    perror("Binding failed");
+    close(sockFd);
+    exit(EXIT_FAILURE);
+  }
+
+  // Listen for incoming connections
+    if (listen(sockFd, 10) == -1) {
+        perror("Listen failed");
+        close(sockFd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port %d\n", PORT);
+
+    // Accept and handle connections
+  while(1){
+      client_sock = malloc(sizeof(int));
+        if (!client_sock) {
+            perror("Malloc failed");
+            continue;
+        }
+
+        *client_sock = accept(sockFd, (struct sockaddr *)&clientaddr, &clientlen);
+        if (*client_sock == -1) {
+            perror("Accept failed");
+            free(client_sock);
+            continue;
+        }
+
+        printf("A connection has been accepted from %s:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+        // Create a new thread for each client
+        if (pthread_create(&thread_id, NULL, handle_client, (void *)client_sock) != 0) {
+            perror("Unable to create the thread");
+            close(*client_sock);
+            free(client_sock);
+        }
+
+        // Detach the thread so that resources are released when it terminates
+        pthread_detach(thread_id);
+   
+  }
+  close(sockFd);
+  return 0;
+  
+}
+
+
+char *server_response(struct Data data)
+{
+    if (data.choice == 1)
+    {
+        // Display catalogue
+        // TODO: Make displayCatalog() function return  type *char
+        DisplayCatalog();
+        return "Catalogue displayed";
+    }
+    else if (data.choice == 2)
+    {
+        // Search for book
+        return searchInFile(data.search);
+    }
+    else if (data.choice == 3)
+    {
+        // Order a book
+        // TODO: Implement orderBook() function
+        return "Book ordered";
+    }
+    else if (data.choice == 4)
+    {
+        // Pay for book
+        // TODO: Make payForItem() function return  type *char
+        PayForItem();
+        return "Payment successful";
+    }
+    else
+    {
+        return "Invalid option";
+    }
+}
