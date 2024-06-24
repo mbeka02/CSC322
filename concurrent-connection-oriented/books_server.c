49a6f22e838009d6f4ac0c9d6b8ee8716fa86339
Roman Njoroge <roman.njoroge@njuguna.com>
2024-06-25 00:23:09 +0300

    feat: Solving issues with server

--- concurrent-connection-oriented/books_server.c
+++ concurrent-connection-oriented/books_server.c
@@ -14,94 +14,106 @@
 #include "../interface.h"
 #define BUFFERSIZE 1024
 
-void *handle_client(void *arg){
-    int sockFd= *(int *)arg;
-    struct sockaddr_in clientaddr; /* client addr */
-    int clientlen=sizeof(clientaddr);/* byte size of client's address */
+void *handle_client(void *arg)
+{
+    int sockFd = *(int *)arg;
+    struct sockaddr_in clientaddr;      /* client addr */
+    int clientlen = sizeof(clientaddr); /* byte size of client's address */
     ssize_t recv_len;
     char buffer[BUFFERSIZE];
 
-
-    while(1){
+    while (1)
+    {
         struct Data data;
-        char* response = malloc(sizeof(char) * 5000);
-        recv_len=read(sockFd,buffer,BUFFERSIZE);
-        if(recv_len < 0){
+        char *response = malloc(sizeof(char) * 5000);
+        recv_len = read(sockFd, buffer, BUFFERSIZE);
+        if (recv_len < 0)
+        {
             perror("unable to read");
             break;
-        }else if(recv_len == 0){
+        }
+        else if (recv_len == 0)
+        {
             printf("\n...client disconnected\n");
             break;
         }
-        struct Data *incoming_data=(struct Data *)buffer;
+        struct Data *incoming_data = (struct Data *)buffer;
         if (incoming_data->choice == 1)
         {
             // Display catalogue
             // TODO: Make displayCatalog() function return  type *char
-            DisplayCatalog(//2,1,3
-                    incoming_data->m,
-                    incoming_data->X,
-                    incoming_data->z,
-                    response
-                    );
-            //return "Catalogue displayed";
+            DisplayCatalog( // 2,1,3
+                incoming_data->m,
+                incoming_data->X,
+                incoming_data->z,
+                response);
+            // return "Catalogue displayed";
         }
         else if (incoming_data->choice == 2)
         {
             // Search for book
-            printf("receiving search query: %s",incoming_data->search);
-            response= SearchBook(incoming_data->search);
+            printf("receiving search query: %s", incoming_data->search);
+            response = SearchBook(incoming_data->search);
         }
         else if (incoming_data->choice == 3)
         {
             // Order a book
             // TODO: Implement orderBook() function
             // return "Book ordered";
-            int orderno=OrderBook(incoming_data->x,incoming_data->y,incoming_data->n);
-            if (orderno == -1) {
+            int orderno = OrderBook(incoming_data->x, incoming_data->y, incoming_data->n);
+            if (orderno == -1)
+            {
                 sprintf(response, "Order Failed! Book %s does not exist", incoming_data->x);
-            } else if (orderno == -2) {
+            }
+            else if (orderno == -2)
+            {
                 response = "Order Failed! Internal Server Error";
-            } else {
+            }
+            else
+            {
                 sprintf(
                     response,
                     "Order successful! Order No: %d",
-                    orderno
-                );
+                    orderno);
             }
         }
         else if (incoming_data->choice == 4)
         {
             // Pay for book
-            int total=PayForBook(incoming_data->orderno,incoming_data->amount);
+            int total = PayForBook(incoming_data->orderno, incoming_data->amount);
 
-            if (total > incoming_data->amount) {
-                response = "Transaction failed! Books cost more than sent amount";
-            } else {
-                response = "Transaction sucessful! Books will arrive in 2 business days";
+            if (total > incoming_data->amount)
+            {
+                sprintf(response, "Transaction failed! Books cost %d but amount sent was %f", total, incoming_data->amount); 
             }
-    }
+            else
+            {
+                sprintf(response, "Transaction sucessful! Books will arrive in 2 business days");
+            }
+        }
         else
         {
             return "Invalid option";
         }
 
-        //response = "...received";
-        //  sendto(sockFd, response, strlen(response), 0, (struct sockaddr *)&clientaddr,clientlen);
-        write(sockFd,response,strlen(response));
-        strcpy(response, "");
+        // response = "...received";
+        //   sendto(sockFd, response, strlen(response), 0, (struct sockaddr *)&clientaddr,clientlen);
+        sleep(5);
+        write(sockFd, response, strlen(response));
+        memset(response,0,sizeof(response));
         free(response);
     }
 
-    free(arg);//free socket descriptor mem
+    free(arg); // free socket descriptor mem
 
     close(sockFd);
     pthread_exit(NULL);
     return NULL;
 }
 
-int main (int argc, char const *argv[]){
-// Get Server Port
+int main(int argc, char const *argv[])
+{
+    // Get Server Port
     if (argc != 2)
     {
         fprintf(stderr, "(Failed) usage: %s <port>\n", argv[0]);
@@ -109,39 +121,41 @@ int main (int argc, char const *argv[]){
     }
     int SERVER_PORT = atoi(argv[1]);
 
- 
- // sockFd is the file descriptor for the socket 
-  // client sock is a ptr to the dynaminc int  for the socket descriptor 
-  int sockFd , *client_sock;
-  //hold the server and client addressses. 
-  struct sockaddr_in server_addr,clientaddr;
-  //hold thread id
-  pthread_t thread_id;
-  //length of the client addr structure
- int clientlen = sizeof(clientaddr);
- //create the socket (TCP)
-  sockFd=socket(AF_INET,SOCK_STREAM,0);
-  if (sockFd==-1){
-    perror("Failed to create the socket");
-    exit(EXIT_FAILURE);
-  }
+    // sockFd is the file descriptor for the socket
+    // client sock is a ptr to the dynaminc int  for the socket descriptor
+    int sockFd, *client_sock;
+    // hold the server and client addressses.
+    struct sockaddr_in server_addr, clientaddr;
+    // hold thread id
+    pthread_t thread_id;
+    // length of the client addr structure
+    int clientlen = sizeof(clientaddr);
+    // create the socket (TCP)
+    sockFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFd == -1)
+    {
+        perror("Failed to create the socket");
+        exit(EXIT_FAILURE);
+    }
 
-  int optval = 1;
-  setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
- //setup the server address 
-  server_addr.sin_family = AF_INET; //specify IPv4
-  server_addr.sin_addr.s_addr=htonl(INADDR_ANY); //bind socket to all available interfaces
-  server_addr.sin_port=htons(SERVER_PORT); //convert port_no to network byte order
+    int optval = 1;
+    setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
+    // setup the server address
+    server_addr.sin_family = AF_INET;                // specify IPv4
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // bind socket to all available interfaces
+    server_addr.sin_port = htons(SERVER_PORT);       // convert port_no to network byte order
 
-  //binding , associate the socket with the given addr and port 
-  if(bind(sockFd,(struct sockaddr *)&server_addr , sizeof(server_addr))==-1){
-    perror("Binding failed");
-    close(sockFd);
-    exit(EXIT_FAILURE);
-  }
+    // binding , associate the socket with the given addr and port
+    if (bind(sockFd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("Binding failed");
+        close(sockFd);
+        exit(EXIT_FAILURE);
+    }
 
-  // Listen for incoming connections
-    if (listen(sockFd, 10) == -1) {
+    // Listen for incoming connections
+    if (listen(sockFd, 10) == -1)
+    {
         perror("Listen failed");
         close(sockFd);
         exit(EXIT_FAILURE);
@@ -150,16 +164,19 @@ int main (int argc, char const *argv[]){
     printf("Server is listening on port %d\n", SERVER_PORT);
 
     // Accept and handle connections
-  while(1){
-    //dynamically allocate space for a client socket descriptor
-      client_sock = malloc(sizeof(int));
-        if (!client_sock) {
+    while (1)
+    {
+        // dynamically allocate space for a client socket descriptor
+        client_sock = malloc(sizeof(int));
+        if (!client_sock)
+        {
             perror("Malloc failed");
             continue;
         }
-       //accept a new connection 
+        // accept a new connection
         *client_sock = accept(sockFd, (struct sockaddr *)&clientaddr, &clientlen);
-        if (*client_sock == -1) {
+        if (*client_sock == -1)
+        {
             perror("Accept failed");
             free(client_sock);
             continue;
@@ -168,7 +185,8 @@ int main (int argc, char const *argv[]){
         printf("A connection has been accepted from %s:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
 
         // Create a new thread for each client
-        if (pthread_create(&thread_id, NULL, handle_client, (void *)client_sock) != 0) {
+        if (pthread_create(&thread_id, NULL, handle_client, (void *)client_sock) != 0)
+        {
             perror("Unable to create the thread");
             close(*client_sock);
             free(client_sock);
@@ -176,13 +194,10 @@ int main (int argc, char const *argv[]){
 
         // Detach the thread so that resources are released when it terminates
         pthread_detach(thread_id);
-   
-  }
-  close(sockFd);
-  return 0;
-  
+    }
+    close(sockFd);
+    return 0;
 }
-
 
 /*char *server_response(struct Data data)
 {
